bf48ac7602996cb0f3e5f405dcdc6f4ccb5392cd
José Guilherme Santos Riberto <joseguilhermeriberto@gmail.com>
2025-03-04 11:47:53 -0300

    [ADD] new website

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('open-dialog-link').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('.cs-dialog').showModal();
+    });
+});
+
+const progressBar = document.getElementById('progress-bar');
+const content = document.getElementById('content');
+let currentWidth = 0;
+
+const interval = setInterval(function(){
+    currentWidth += 20;
+    progressBar.style.width = currentWidth + '%';
+    
+    if(currentWidth >= 100){
+        clearInterval(interval);
+        content.classList.remove('hidden');
+        progressBar.parentElement.classList.add('hidden');
+    }
+}, 500);
